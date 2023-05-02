96084dd30b6b1528d475ae4b869c3a3ca24b75af
octospacc <octo@tutamail.com>
2023-05-02 13:06:24 +0200

    Upd

--- assets/Site/CheckboxHack-Polyfill.js
+++ assets/Site/CheckboxHack-Polyfill.js
@@ -4,6 +4,13 @@ var Done = false;
 function Polyfill() {
 if (!Done) {
 	// TODO
+	var Boxs = document.querySelectorAll('input[type~="checkbox"].ToggleDisplay, input[type~="radio"].ToggleDisplay');
+	for (var i=0; i<Boxs.length; i++) {
+		Boxs[i].onclick = function(){
+			alert(1);
+			this.nextElementSibling.hidden = !this.checked;
+		};
+	};
 
 	Done = true;
 };
--- wiki/index.md
+++ wiki/index.md
@@ -1,3 +1,9 @@
 # WikiHands
 
+<div Lang=en>
+
 In construction.
+
+* Test
+
+</div>
